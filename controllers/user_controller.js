cf9e7f61c9be6a244cb4597ed8cdc49edcdf0203
DhananjaySingh <singhdhananjay8969@gmail.com>
2020-04-09 00:01:31 +0530

    added signin and signup form (final commit)

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,13 +5,13 @@ module.exports.post=function(req,res){
     return res.render('../views/user_post.ejs',{});
 }
 module.exports.signIn=function(req,res){
-    return res.render('user_signIn',{
-        title:"socializer/signIn"
+    return res.render('user_sign_in',{
+        title:"socializer/sign-in"
     });
 }
 module.exports.signUp=function(req,res)
 {
-    return res.render('user_signUp',{
-        title:"socializer/signUp"
+    return res.render('user_sign_up',{
+        title:"socializer/sign-Up"
     })
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,6 @@ const router=express.Router();
 const userController=require('../controllers/user_controller');
 router.get('/profile',userController.profile);
 router.get('/post',userController.post);
-router.get('/signIn',userController.signIn);
-router.get('/signUp',userController.signUp);
+router.get('/sign-in',userController.signIn);
+router.get('/sign-up',userController.signUp);
 module.exports=router;
