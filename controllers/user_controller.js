e552ee49ccd4ed1617799102d55a6204012dc13c
DhananjaySingh <singhdhananjay8969@gmail.com>
2020-04-08 23:33:30 +0530

    added sign in and sign up form

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,3 +4,14 @@ module.exports.profile=function(req,res){
 module.exports.post=function(req,res){
     return res.render('../views/user_post.ejs',{});
 }
+module.exports.signIn=function(req,res){
+    return res.render('user_signIn',{
+        title:"socializer/signIn"
+    });
+}
+module.exports.signUp=function(req,res)
+{
+    return res.render('user_signUp',{
+        title:"socializer/signUp"
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,6 @@ const router=express.Router();
 const userController=require('../controllers/user_controller');
 router.get('/profile',userController.profile);
 router.get('/post',userController.post);
+router.get('/signIn',userController.signIn);
+router.get('/signUp',userController.signUp);
 module.exports=router;
