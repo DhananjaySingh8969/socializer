4417de067cb9706d5e2df4242ef72f31c017abcf
DhananjaySingh <singhdhananjay8969@gmail.com>
2020-04-09 23:07:24 +0530

    sign up (account creation) completed

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,4 @@
+const User=require('../models/user');
 module.exports.profile=function(req,res){
     return res.render('../views/user_profile.ejs',{});
 }
@@ -5,23 +6,47 @@ module.exports.post=function(req,res){
     return res.render('../views/user_post.ejs',{});
 }
 module.exports.signIn=function(req,res){
-    return res.render('user_sign_in',{
+    return res.render('../views/user_sign_in.ejs',{
         title:"socializer/sign-in"
     });
 }
 module.exports.signUp=function(req,res)
 {
-    return res.render('user_sign_up',{
+    return res.render('../views/user_sign_up',{
         title:"socializer/sign-Up"
     })
 }
 
-//user sign up
-module.exports.create=function(req,res){
-       //todo later
-};
+//user create
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user)
+    {
+        if(err)
+        {
+            console.log('error in finding user in signing up'); 
+            return
+        }
+
+        if (!user)
+        {
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+                console.log('account has been created');
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            console.log('user already exist');
+            return res.redirect('back');
+        }
+
+    });
+}
 
 //user sign in create a session
 module.exports.createSession=function(req,res){
-    //todo later
-};
+
+}
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,6 @@ router.get('/profile',userController.profile);
 router.get('/post',userController.post);
 router.get('/sign-in',userController.signIn);
 router.get('/sign-up',userController.signUp);
+router.post('/create',userController.create);
+router.post('/create-session',userController.createSession);
 module.exports=router;
