8432d6a37f3280f63356453d9ce57a06d8b1765f
DhananjaySingh <singhdhananjay8969@gmail.com>
2020-04-24 11:42:38 +0530

    displayed posts on home

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,20 @@
+const Post=require('../models/post');
+const User=require('../models/user');
 module.exports.home=function(req,res)
 {   
     // console.log(req.cookies);//accessing cookies from browser
-    res.cookie('ui',13);//sending cookies to browser
-    return res.render('home',{
-        title:"HOME"
-    })
+    // res.cookie('ui',13);//sending cookies to browser
+    
+    Post.find({}).populate('user').exec(function(err,posts){
+        if(err)
+        {
+            console.log('error in fetching posts');
+        }
+        // console.log(posts);
+        return res.render('home',{
+            title:"HOME",
+            posts:posts
+        })
+    });
+    
 }
