2a6965263fd4a57a297f7c73cc314c3ce4c11b14
DhananjaySingh <singhdhananjay8969@gmail.com>
2020-04-25 00:34:42 +0530

    delete comment option has been added

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,5 @@
 const Post=require('../models/post');
-
+const Comment=require('../models/comment');
 module.exports.create=function(req,res)
 {   
     console.log(req.user);
@@ -14,4 +14,34 @@ module.exports.create=function(req,res)
             
             return res.redirect('back');
         })
+}
+module.exports.destroy=function(req,res)
+{   
+   //deleting the post
+    console.log(req.params.id);
+    Post.findById(req.params.id,
+        function(err,post){
+           if(err)
+           {
+               console.log('error in deleting the post');
+               return ;
+           }
+           //.id means converting the Object id into string
+           if(post.user==req.user.id)
+           {
+               post.remove();
+               Comment.deleteMany({post:req.params.id},function(err)
+               {
+                     if(err)
+                     {
+                         console.log("error in deleting comments of the post");
+                         return ;
+                     }
+                     return res.redirect('back');
+               });
+           }else{
+              return res.redirect('back');
+           }
+    });
+  
 }
