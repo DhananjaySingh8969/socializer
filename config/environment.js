425d41a9e4b4079ef70a971f06c0ee3dd69c6813
DhananjaySingh <singhdhananjay8969@gmail.com>
2020-05-13 11:19:02 +0530

    set environment variable for prodution

--- config/environment.js
+++ config/environment.js
@@ -1,28 +1,47 @@
+// console.log(process.env.SOCIALIZER_GOOGLE_CLIENT_ID,process.env.SOCIALIZER_GOOGLE_CLIENT_SECRET,process.env.SOCIALIZER_GOOGLE_CALLBACK_URL);
 const development={
    name:'development',
-   asset_path:'/assets',
-   session_cookie_key:'blahsomething',
-   db:'socializer_devlopment',
+   asset_path:process.env.SOCIALIZER_ASSETS_PATH,
+   session_cookie_key:process.env.SOCIALIZER_SESSION_COOKIE_KEY,
+   db:process.env.SOCIALIZER_DB,
    smtp:{
         service: 'gmail',
         host: 'smtp.gmail.com',
         port: 587,
         secure: false,
         auth:{
-            user: 'singhdhananjay8969@gmail.com',
-            pass: '#8969#7352#Thanos'
+            user: process.env.SOCIALIZER_EMAIL,
+            pass:process.env.SOCIALIZER_EMAIL_PASSWORD
         }
     },
-    google_client_id:"351776873665-41ecj4a799kfesrpnvvuib2ukrqll05m.apps.googleusercontent.com",
-    google_client_secret:"CX957cquKheFrfS2_15r2_sm",
-    google_callback_url:"http://localhost:8000/users/auth/google/callback",
-    jwt_secret:'socializer'
+    google_client_id:process.env.SOCIALIZER_GOOGLE_CLIENT_ID,
+    google_client_secret:process.env.SOCIALIZER_GOOGLE_CLIENT_SECRET,
+    google_callback_url:process.env.SOCIALIZER_GOOGLE_CALLBACK_URL,
+    jwt_secret:process.env.SOCIALIZER_JWT_SECRET
 }
 
 const production={
-    name:"production"
+   name:"production",
+   asset_path:process.env.SOCIALIZER_ASSETS_PATH,
+   session_cookie_key:process.env.SOCIALIZER_SESSION_COOKIE_KEY,
+   db:process.env.SOCIALIZER_DB,
+   smtp:{
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth:{
+            user: process.env.SOCIALIZER_EMAIL,
+            pass:process.env.SOCIALIZER_EMAIL_PASSWORD
+        }
+    },
+    google_client_id:process.env.SOCIALIZER_GOOGLE_CLIENT_ID,
+    google_client_secret:process.env.SOCIALIZER_GOOGLE_CLIENT_SECRET,
+    google_callback_url:process.env.SOCIALIZER_GOOGLE_CALLBACK_URL,
+    jwt_secret:process.env.SOCIALIZER_JWT_SECRET
 }
 
 
 
-module.exports=development;
+
+module.exports=eval(process.env.SOCIALIZER_ENVIRONMENT)==undefined?development:eval(process.env.SOCIALIZER_ENVIRONMENT);
